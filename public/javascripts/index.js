3f4bf448198ec60217ce795ca589cf3274b92bce
Slki Shin <slki.shin@samsung.com>
2018-07-09 09:58:49 +0900

    add acetone, toluene chart
    Signed-off-by: Slki Shin <slki.shin@samsung.com>

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -114,13 +114,14 @@ $(document).ready(function () {
   }
   //Get the context of the canvas element we want to select
   var ctx = document.getElementById("myChart").getContext("2d");
+  var ctx2 = document.getElementById("myChart2").getContext("2d");
   var optionsNoAnimation = { animation: false }
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: data,
     options: basicOption
   });
-  var myLineChart2 = new Chart(ctx, {
+  var myLineChart2 = new Chart(ctx2, {
     type: 'line',
     data: data2,
     options: basicOption2
